a3fd3b6901fa8937bba8b27d7494525733632936
obhasin <63110355+obhasin@users.noreply.github.com>
2025-02-10 06:57:59 +0100

    Update Buzzy Bee Classification Analysis.R

--- Buzzy Bee Classification Analysis.R
+++ Buzzy Bee Classification Analysis.R
@@ -133,6 +133,9 @@ video3.IA_3o <- Tv3$subject_id[Tv3$interesting.animals >= 0.7]
 
 video.IA_not_in_video3.IA = video.IA[!(video.IA %in% video3.IA_1o | video.IA %in% video3.IA_2o  | video.IA %in% video3.IA_3o)]
 
+# Set working directory
+setwd("/Users/Oria/Documents/ULB/PhD/Zooniverse/Zoo_Data_Analysis")
+
 # Create playlist
 #subject_id = video.IA_not_in_video3.IA
 subject_id = video3.IA_3o
